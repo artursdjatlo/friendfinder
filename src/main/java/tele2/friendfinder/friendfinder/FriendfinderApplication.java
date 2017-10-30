eb4de97d6e0b7cfea5f6eadf8ca7630606dc356f
Gatis Jermacāns <gatis.jermacans@gmail.com>
2017-10-30 15:20:41 +0200

    Add files via upload

---
+++ src/main/java/tele2/friendfinder/friendfinder/FriendfinderApplication.java
@@ -0,0 +1,12 @@
+package tele2.friendfinder.friendfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendfinderApplication.class, args);
+	}
+}
---
+++ src/test/java/tele2/friendfinder/friendfinder/FriendfinderApplicationTests.java
@@ -0,0 +1,16 @@
+package tele2.friendfinder.friendfinder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FriendfinderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
